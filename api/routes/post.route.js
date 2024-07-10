9313f3ea25f1e15e189ccf0e36b3254898967b9b
jatingarg0410 <gargjatin0410@gmail.com>
2024-07-11 01:57:02 +0530

    created post model and controller

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,13 +1,19 @@
 import express from 'express'
+import { verifyToken } from '../middleware/verifyToken.js'
+import {
+  addPost,
+  deletePost,
+  getPost,
+  getPosts,
+  updatePost,
+} from '../controllers/post.controller.js'
 
 const router = express.Router()
-// router.post('/register', (req, res) => {
-//   console.log('first router worls !')
-// })
-router.get('/love', (req, res) => {
-  res.send('first router works !')
-})
-// router.post('/logout', (req, res) => {
-//   console.log('first router worls !')
-// })
+
+router.get('/', getPosts)
+router.get('/:id', getPost)
+router.post('/', verifyToken, addPost)
+router.put('/:id', verifyToken, updatePost)
+router.delete('/:id', verifyToken, deletePost)
+
 export default router
